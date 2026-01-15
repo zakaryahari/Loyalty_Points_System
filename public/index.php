a53b35d8e6d1fa12e4dbece771c19c10d49b1cd7
zakarya hari <zakariahari42@gmail.com>
2026-01-15 10:20:43 +0100

    Ajouté un system de routage inisiale pour la page index .

--- public/index.php
+++ public/index.php
@@ -2,9 +2,19 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Controllers\HomeController;
+use App\Core\Router;
 
-$controller = new HomeController();
-$controller->index();
+// $controller = new HomeController();
+// $controller->index();
+
+$Router = new Router();
+
+$Router->add('/brief-11/public/' , 'HomeController' , 'index');
+$Router->add('/brief-11/' , 'HomeController' , 'index');
+$Router->add('index.php' , 'HomeController' , 'index');
+
+$url = parse_url($_SERVER['REQUEST_URI'] , PHP_URL_PATH);
+
+$Router->dispatch($url);
 
 ?>
