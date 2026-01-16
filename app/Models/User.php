87bd5edd7d3db575fc52eaf10c655b974e7c0bbd
zakarya hari <zakariahari42@gmail.com>
2026-01-16 11:21:09 +0100

    implémentation du modèle User et de l'authentification
    Implémentation de la méthode de login et de vérification des identifiants. / aussi la methode de Updatepoints .

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,14 +59,67 @@ class User {
     }
 
     public function register(array $data): bool {
-        
+        try {
+            $this->db->getConnection()->beginTransaction();
+
+            $sql = "INSERT INTO users (email, password_hash ,name , total_points ,createdat) VALUES (:email, :password_hash , :name ,:total_points ,:createdat )";
+            $query = $this->db->getConnection()->prepare($sql);
+            $query->bindValue(":email", $data['email']);
+            $hashed_pw = password_hash($password, PASSWORD_BCRYPT);
+            $query->bindValue(":password_hash", $hashed_pw);
+            $query->bindValue(":name", $data['name']);
+            $query->bindValue(":total_points", $data['total_points']);
+            $query->bindValue(":createdat", $data['createdat']);
+            $query->execute();
+            $newId = $db->lastInsertId();
+
+            $this->id = (int) $newId;
+
+            $this->db->getConnection()->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->db->getConnection()->rollBack();
+            return false;
+        }
     }
 
     public function login(string $email, string $password): bool {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->bindValue(":email", $email);
+        $query->execute();
+        $result = $query->fetch();
+
+        if (!$result) {
+            return false;
+        }
+        else{
         
+            $this->email = $result['email'];
+
+            if (!$this->verifyPassword($password)) {
+                return false;
+            }
+            return true;
+        }
     }
 
     public function updatePoints(int $userId, int $points): bool {
-        
+        try {
+            $this->db->getConnection()->beginTransaction();
+
+            $sql = "UPDATE users SET total_points = :total_points WHERE id = :id" ;
+            $query = $this->db->getConnection()->prepare($sql);
+            $query->bindValue(":id", $userId , PDO::PARAM_INT);
+            $query->bindValue(":total_points", $points , PDO::PARAM_INT);
+            $query->execute();
+
+            $this->db->getConnection()->commit();
+            return true;
+            
+        } catch (Exception $e) {
+            $this->db->getConnection()->rollBack();
+            return false;
+        }
     }
 }
