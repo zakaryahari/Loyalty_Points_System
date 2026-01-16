50de539a6183a995bb3cbdd70e71d740b5c87837
zakarya hari <zakariahari42@gmail.com>
2026-01-16 11:22:39 +0100

    implémentation du modèle Reward et gestion des stocks
    Création du catalogue des récompenses (méthodes getAll et getById). / Ajout de la logique de vérification de disponibilité (stock limité vs illimité).

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -43,19 +43,77 @@ private $db;
         $this->stock = $stock; 
     }
 
-    public function getAll(): array {
-        
+    public function getAll(): ?array {
+        $sql = "SELECT * FROM rewards";
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->execute();
+        $result = $query->fetchAll();
+
+        if (!$result) {
+            return Null;
+        }
+        else{
+            return $result;
+        }
     }
 
-    public function getById(int $id): ?object {
-        
+    public function getById(int $id): ?self {
+        $sql = "SELECT * FROM rewards WHERE id = :id";
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->bindValue(":id", $id ,PDO::PARAM_INT);
+        $query->execute();
+        $result = $query->fetch();
+
+        if (!$result) {
+            return Null;
+        }
+        else{
+            return new self($this->db,$result);
+        }
     }
 
     public function isAvailable(int $id): bool {
-        
+        $sql = "SELECT stock FROM rewards WHERE id = :id";
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->bindValue(":id", $id ,PDO::PARAM_INT);
+        $query->execute();
+        $result = $query->fetch();
+
+        if (!$result) {
+            return false;
+        }
+        else if ($result['stock'] > 0 || $result['stock'] == -1) {
+            return true;
+        }
     }
 
     public function decrementStock(int $id): bool {
-        
+        $sql_check_stock = "SELECT stock FROM rewards WHERE id = :id";
+        $query_check_stock = $this->db->getConnection()->prepare($sql_check_stock);
+        $query_check_stock->bindValue(":id", $id ,PDO::PARAM_INT);
+        $query_check_stock->execute();
+        $result_check_stock = $query_check_stock->fetch();
+
+        if ($result_check_stock['stock'] > 0) {
+            try {
+                $this->db->getConnection()->beginTransaction();
+                  
+                $sql = "UPDATE rewards SET stock = stock - 1  WHERE id = :id" ;
+                $query = $this->db->getConnection()->prepare($sql);
+                $query->bindValue(":id", $id , PDO::PARAM_INT);
+                $query->execute();
+
+                $this->db->getConnection()->commit();
+                return true;
+                
+            } catch (Exception $e) {
+                $this->db->getConnection()->rollBack();
+                return false;
+            }        
+        }
+        elseif ($result_check_stock['stock'] == -1) {
+            return true;
+        }
+        return false;
     }
 }
